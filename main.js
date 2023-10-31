5c7bbdaa6ab95714aac067f2c9dde277f839c242
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-10-31 12:55:26 +0100

    divider section with rwd

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 import './styles/main.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css/autoplay'
+
+
+    const swiper = new Swiper('.swiper', {
+        speed: 200,
+        spaceBetween: 32,
+        slidesPerView: 1,
+        autoplay: {
+            delay: 5000,
+          },
+          breakpoints: {
+            768: {
+                slidesPerView: 2,
+            }
+        },
+        modules: [
+            Autoplay,
+        ]
+       
+     
+        // breakpoints: {
+        //     768: {
+        //         slidesPerView: 1,
+        //         spaceBetween: 20,
+        //     }
+        // },
+      });
+
+
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
@@ -21,4 +52,3 @@ import { setupCounter } from './counter.js'
 //   </div>
 // `
 
-setupCounter(document.querySelector('#counter'))
