e6fe42a51405b1c9bf362eebbcfd0a1aff0902ea
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-11-05 17:23:45 +0100

    form validation

--- main.js
+++ main.js
@@ -71,43 +71,148 @@ import 'swiper/css/autoplay'
 
 
 
+ 
+    //   const fieldset = document.querySelector('.app-fieldset')
+      const warningPrivacyPolicy = document.querySelector('.warning-message')
+    //   const checkbox = document.querySelector('.app-input-checkbox')
+    //   const contactFormInputs = document.querySelectorAll('.app-form-essential')
+    //   const contactFormCheckbox = document.querySelector('#data-processing-checkbox')
+    //   const contactFormTextArea = document.querySelector('.app-textarea')
+     const contactFormSendBtn = document.querySelector('.contact-section__submit-btn')
+     const contactFormWarningMsg = document.querySelectorAll('.empty-field-warning-message')
+     const form = document.querySelector('.contact-form');
+    //  const EMAIL_REGEX =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+     const selectInput = document.querySelector('.app-select');
+     const emailInput = document.querySelector('.app-input-email');
+     const textareaInput = document.querySelector('.app-textarea');
+     const checkboxInput = document.querySelector('.app-input-checkbox');
 
-      const contactFormInputs = document.querySelectorAll('.app-form-essential')
-      const contactFormCheckbox = document.querySelector('#data-processing-checkbox')
-      const contactFormTextArea = document.querySelector('.app-textarea')
-      const contactFormSendBtn = document.querySelector('.contact-section__submit-btn')
-      const contactFormWarningMsg = document.querySelectorAll('.empty-field-warning-message')
-      const fieldset = document.querySelector('.app-fieldset')
 
-   
+      const isRequired = value => value === '' ? false : true;
+      const isSelected = value => value > 0  
+      const isBetween = (length, min, max) => length < min || length > max ? false : true;
+      const isEmailValid = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+    const isChecked = value => value.checked
 
-      const handleInputValidation = () => {
-        
-       contactFormInputs.forEach((input)=> {
-         if(input.value === '') {
-           input.classList.add('empty-field')
-           contactFormWarningMsg.forEach((message)=> {
-            message.style.display = 'block'
-           })
-        //    contactFormCheckbox.style.borderColor = 'red'
-          } else {
-            input.classList.remove('empty-field')
-            // contactFormCheckbox.style.borderColor = '#D1D5DB'
-          }
-       })
+
+
+
+      const showError = (input, message) => {
+        const errorMsg = input.nextSibling.nextSibling;
+        errorMsg.style.display = 'block';
+        errorMsg.textContent = message;
+        input.classList.add('error');
+        // console.log(in
+      }
+     
+      const showSuccess = (input) => {
+        const errorMsg = input.nextSibling.nextSibling;
+        errorMsg.style.display = 'none';
+        errorMsg.textContent = '';
+        input.classList.remove('error');
+        input.classList.add('success');
       }
 
-    //   const handleTextAreaValidation = () => {
-    //         if(contactFormTextArea.value === '') {
-    //             contactFormTextArea.classList.add('empty-field')
-    //         }
+
+      const checkTextArea = () => {
+        let valid = false;
+        const min = 5,
+              max = 350;
+        const UsersMessage = textareaInput.value.trim();
+
+        if(!isRequired(UsersMessage)) {
+            showError(textareaInput, 'To pole nie moze być puste')
+        } else if (!isBetween(UsersMessage.length, min, max)) {
+            showError(textareaInput, 'Twoja wiadomość jest za krótka') 
+        } else {
+            showSuccess(textareaInput);
+            valid = true;
+        }
+
+        return valid;
+        
+      }
+
+
+      textareaInput.addEventListener('keyup', checkTextArea)
  
 
-    //   }
-
      
 
-      fieldset.addEventListener('click', handleInputValidation);
-      contactFormSendBtn.addEventListener('click', handleInputValidation);
-     
-    //   contactFormSendBtn.addEventListener('click', handleTextAreaValidation);
+
+      const checkEmailInput = () => {
+        let valid = false;
+        const email = emailInput.value.trim();
+        if (!isRequired(email)) {
+            showError(emailInput, 'To pole nie moze być puste')
+        } else if (!isEmailValid(email)) {
+            showError(emailInput, 'Niepoprawny adres e-mail')
+        } else {
+            showSuccess(emailInput);
+            valid = true;
+        }
+        return valid
+      }
+
+      emailInput.addEventListener('keyup', checkEmailInput)
+
+      const checkSelectInput = () => {
+        let valid = false;
+        const select = selectInput.value;
+        if(!isSelected(select)) {
+            showError(selectInput, 'Musisz wybrać kategorie')
+        } else {
+            showSuccess(selectInput);
+            valid = true;
+        }
+
+        return valid
+      }
+
+      selectInput.addEventListener('change', checkSelectInput)
+
+
+      const handleCheckBox = () => {
+            let valid = false;
+            const checkbox = checkboxInput;
+            if(!isChecked(checkbox)) {
+                warningPrivacyPolicy.style.display = 'block'
+            } else {
+                warningPrivacyPolicy.style.display = 'none'
+                valid = true;
+            }
+
+            return valid
+        }
+
+        checkboxInput.addEventListener('click', handleCheckBox)
+
+           const errors = [];
+
+
+
+ form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let isTextFieldValid = checkTextArea();
+        let isEmailValid = checkEmailInput();
+        let isSelected = checkSelectInput()
+        let isChecked = handleCheckBox()
+
+        let isFormValid = isTextFieldValid &&
+        isEmailValid &&
+        isSelected &&
+        isChecked;
+
+        if(isFormValid) {
+            console.log('OK');
+        }
+      })
+
+
+
+
+
