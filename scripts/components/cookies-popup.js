30aa4789b424d3d838c8f11f239df3da45dfef43
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-11-10 14:42:41 +0100

    added cookies popup + js & meta tags

---
+++ scripts/components/cookies-popup.js
@@ -0,0 +1,38 @@
+ import Cookies from 'js-cookie'
+
+
+const setupCookies = () => {
+
+    const cookiesPopupWindow = document.querySelector('.cookies-popup');
+    const acceptCookiesBtn = document.querySelector('.cookies-popup-buttons__accept-btn');
+    const declineCookiesBtn = document.querySelector('.cookies-popup-buttons__decline-btn');
+    
+    
+    if(Cookies.get('acceptCookies')) {
+        return;
+    } 
+
+    setTimeout(()=> {
+        cookiesPopupWindow.classList.add('cookies-popup--visible');
+        
+    },1500)
+        
+
+    const acceptCookies = () => {
+        Cookies.set('acceptCookies', 'true')
+        cookiesPopupWindow.classList.remove('cookies-popup--visible')
+    }
+
+    const declineCookies = () => {
+        Object.keys(Cookies.get()).forEach((cookieName)=> {
+            Cookies.remove(cookieName)
+        });
+        cookiesPopupWindow.classList.remove('cookies-popup--visible')
+    }
+
+    acceptCookiesBtn.addEventListener('click', acceptCookies)
+    declineCookiesBtn.addEventListener('click', declineCookies)
+}
+
+
+export{ setupCookies };
--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,14 @@ import 'aos/dist/aos.css';
 import { setupCarousel } from './components/carousel';
 import { setupFormValidation } from './components/form-validation';
 import { setupNavigation } from './components/navigation';
+import { setupCookies } from './components/cookies-popup';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupCarousel();
   setupFormValidation();
   setupNavigation();
+  setupCookies();
   AOS.init();
 });
+
+
