d711c7f76d710f9830d1b7b5404cb82ac58cdc83
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-11-12 15:31:45 +0100

    formated code

--- scripts/components/cookies-popup.js
+++ scripts/components/cookies-popup.js
@@ -1,38 +1,32 @@
- import Cookies from 'js-cookie'
-
+import Cookies from 'js-cookie';
 
 const setupCookies = () => {
+  const cookiesPopupWindow = document.querySelector('.cookies-popup');
+  const acceptCookiesBtn = document.querySelector('.cookies-popup-buttons__accept-btn');
+  const declineCookiesBtn = document.querySelector('.cookies-popup-buttons__decline-btn');
 
-    const cookiesPopupWindow = document.querySelector('.cookies-popup');
-    const acceptCookiesBtn = document.querySelector('.cookies-popup-buttons__accept-btn');
-    const declineCookiesBtn = document.querySelector('.cookies-popup-buttons__decline-btn');
-    
-    
-    if(Cookies.get('acceptCookies')) {
-        return;
-    } 
+  if (Cookies.get('acceptCookies')) {
+    return;
+  }
 
-    setTimeout(()=> {
-        cookiesPopupWindow.classList.add('cookies-popup--visible');
-        
-    },1500)
-        
+  setTimeout(() => {
+    cookiesPopupWindow.classList.add('cookies-popup--visible');
+  }, 1500);
 
-    const acceptCookies = () => {
-        Cookies.set('acceptCookies', 'true')
-        cookiesPopupWindow.classList.remove('cookies-popup--visible')
-    }
+  const acceptCookies = () => {
+    Cookies.set('acceptCookies', 'true');
+    cookiesPopupWindow.classList.remove('cookies-popup--visible');
+  };
 
-    const declineCookies = () => {
-        Object.keys(Cookies.get()).forEach((cookieName)=> {
-            Cookies.remove(cookieName)
-        });
-        cookiesPopupWindow.classList.remove('cookies-popup--visible')
-    }
+  const declineCookies = () => {
+    Object.keys(Cookies.get()).forEach((cookieName) => {
+      Cookies.remove(cookieName);
+    });
+    cookiesPopupWindow.classList.remove('cookies-popup--visible');
+  };
 
-    acceptCookiesBtn.addEventListener('click', acceptCookies)
-    declineCookiesBtn.addEventListener('click', declineCookies)
-}
+  acceptCookiesBtn.addEventListener('click', acceptCookies);
+  declineCookiesBtn.addEventListener('click', declineCookies);
+};
 
-
-export{ setupCookies };
+export { setupCookies };
--- scripts/main.js
+++ scripts/main.js
@@ -16,5 +16,3 @@ document.addEventListener('DOMContentLoaded', () => {
   setupModal();
   AOS.init();
 });
-
-
